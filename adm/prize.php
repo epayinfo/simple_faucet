fceb8c26f63b61515fff095220835d1bc7fb2279
ePay.info <info@epay.info>
2016-03-27 04:24:58 +0430

    Redesign
    Signed-off-by: ePay.info <info@epay.info>

--- adm/prize.php
+++ adm/prize.php
@@ -164,7 +164,16 @@ END;
 
 
 ?>
-<table width="100%" class="table table-hover personal-task">
+
+
+
+
+
+
+
+
+
+<table class="table table-hover table-striped">
 
     <tr>
       <th width="25%" height="34">Prize</th>
@@ -189,94 +198,96 @@ $db->query("select * from tbl_prize order by chance desc");
 while($res=$db->fetchArray()){
 ?>
     <tr>
-      <td width="25%"><?php echo $res['prize']; ?> Satoshi</td>
+      <td width="25%"><?php echo $res['prize']; ?></td>
       <td width="17%"><?php echo $chancer[$res['prize']]; ?></td>
       <td width="58%">
       
-      Chance Up => 
+<Div class="row text-left" style="margin-bottom:5px;">
+	  
+      <Div class="col-md-3">Chance Up =>  </Div>
       
-      	<a href="?a=prize_up&pid=<?php echo $res['prize_id'];?>" title="Chance up x1">x1</a>
-      
-      
-      	<a href="?a=prize_upd&pid=<?php echo $res['prize_id'];?>" title="Chance up x2">x2</a>
-      
-      	<a href="?a=prize_upd5&pid=<?php echo $res['prize_id'];?>" title="Chance up x5">x5</a>
+      	<a href="?a=prize_up&pid=<?php echo $res['prize_id'];?>" title="Chance up x1" class="btn btn-success btn-xs">x1</a>
       
       
-      	<a href="?a=prize_upd10&pid=<?php echo $res['prize_id'];?>" title="Chance up x10">x10</a>
+      	<a href="?a=prize_upd&pid=<?php echo $res['prize_id'];?>" title="Chance up x2" class="btn btn-success btn-xs">x2</a>
+      
+      	<a href="?a=prize_upd5&pid=<?php echo $res['prize_id'];?>" title="Chance up x5" class="btn btn-success btn-xs">x5</a>
       
       
-      	<a href="?a=prize_upd20&pid=<?php echo $res['prize_id'];?>" title="Chance up x20">x20</a>
+      	<a href="?a=prize_upd10&pid=<?php echo $res['prize_id'];?>" title="Chance up x10" class="btn btn-success btn-xs">x10</a>
       
       
-      	<a href="?a=prize_upd100&pid=<?php echo $res['prize_id'];?>" title="Chance up x20">x100</a>
-      
-      	<a href="?a=prize_upd200&pid=<?php echo $res['prize_id'];?>" title="Chance up x20">x200</a>
-      
-      	<a href="?a=prize_upd1000&pid=<?php echo $res['prize_id'];?>" title="Chance up x20">x1000</a>
+      	<a href="?a=prize_upd20&pid=<?php echo $res['prize_id'];?>" title="Chance up x20" class="btn btn-success btn-xs">x20</a>
       
       
+      	<a href="?a=prize_upd100&pid=<?php echo $res['prize_id'];?>" title="Chance up x20" class="btn btn-success btn-xs">x100</a>
+      
+      	<a href="?a=prize_upd200&pid=<?php echo $res['prize_id'];?>" title="Chance up x20" class="btn btn-success btn-xs">x200</a>
+      
+      	<a href="?a=prize_upd1000&pid=<?php echo $res['prize_id'];?>" title="Chance up x20" class="btn btn-success btn-xs">x1000</a>
+      
+      
+      
+</Div>
       
 
       
-
-      
       
 
-      
+      <Div class="row">
       <?php if($res['chance']>1){?>
-      <br>
-      Chance Down => 
-      	<a href="?a=prize_down&pid=<?php echo $res['prize_id'];?>" title="Chance down low">x1</a>
+       
+      <Div class="col-md-3"> Chance Down =>  </Div>
+      	<a href="?a=prize_down&pid=<?php echo $res['prize_id'];?>" title="Chance down low" class="btn btn-warning btn-xs">x1</a>
       <?php } ?>
       
       
       <?php if($res['chance']>2){?>
-      	<a href="?a=prize_downd&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x2</a>
+      	<a href="?a=prize_downd&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x2</a>
       <?php } ?>
       
       
       
       <?php if($res['chance']>5){?>
-      	<a href="?a=prize_downd5&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x5</a>
+      	<a href="?a=prize_downd5&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x5</a>
       <?php } ?>
       
       
       <?php if($res['chance']>10){?>
-      	<a href="?a=prize_downd10&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x10</a>
+      	<a href="?a=prize_downd10&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x10</a>
       <?php } ?>
       
       
       <?php if($res['chance']>20){?>
-      	<a href="?a=prize_downd20&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x20</a>
+      	<a href="?a=prize_downd20&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x20</a>
       <?php } ?>
       
       
       
       <?php if($res['chance']>100){?>
-      	<a href="?a=prize_downd100&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x100</a>
+      	<a href="?a=prize_downd100&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x100</a>
       <?php } ?>
       
       
       <?php if($res['chance']>200){?>
-      	<a href="?a=prize_downd200&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x200</a>
+      	<a href="?a=prize_downd200&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x200</a>
       <?php } ?>
       
       
       <?php if($res['chance']>1000){?>
-      	<a href="?a=prize_downd1000&pid=<?php echo $res['prize_id'];?>" title="Chance down high">x1000</a>
+      	<a href="?a=prize_downd1000&pid=<?php echo $res['prize_id'];?>" title="Chance down high" class="btn btn-warning btn-xs">x1000</a>
       <?php } ?>
       
       
       
-      <br>
+      </Div>
 
       
+       <Div class="row">
       
+      	<Div class="col-md-3"> <a href="?a=prize_remove&pid=<?php echo $res['prize_id'];?>"title="Remove" class="btn btn-danger btn-xs">Remove</a> </Div>
       
-      	<a href="?a=prize_remove&pid=<?php echo $res['prize_id'];?>"title="Remove">Remove</a>
-      
-      
+      </Div>
       
       
       </td>
@@ -286,33 +297,49 @@ while($res=$db->fetchArray()){
 </table>
 
 
-<form action="" method="post">
 
 
 
-<table width="100%" border="0">
-                  <tr align="center">
-                    <th width="50%" align="center">Prize In satoshi</th>
-                    <td width="50%">
-                    
-                    <input type="text" class="text" name="prize" >
-                    
-                    </td>
-                  </tr>
 
 
-                  <tr>
-                    <td colspan="2" align="center">
-                <button type="submit" name="new_price" class="button">Add</button>
-                  
-                    </td>
-                  </tr>
-                </table>
-</form>
+<Div class="well">
 
-<center>
-<a href="?a=apply"  class="button" style="width:200px;">Apply New prizes</a>
-</center>
+	<form action="" method="post">
+		
+		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Prize In satoshi</label>
+			<input type="number" class="form-control" name="prize" >
+		</div>
+		
+		<button type="submit" name="new_price" class="btn btn-success col-md-3 col-md-push-4">Add</button>
+
+		<div class="clearfix"></div>
+		
+	
+	</form>
+
+
+</Div>
+
+
+<Div class="well">
+
+	<a href="?a=apply"  class="btn btn-primary col-md-3 col-md-push-4 btn-lg">Apply New prizes</a>
+
+
+	<div class="clearfix" style="margin-top:20px;"></div>
+<br>
+
+	<Div class="alert alert-info">
+		Remember to apply new prizes after change to take effect.
+	
+	</Div>
+
+
+
+</Div>
+
 
 
 <?php } include("footer.php") ?>
