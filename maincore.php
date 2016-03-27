b7ff8626aba4ab3edb60eef7d30720c62c4e0454
ePay.info <info@epay.info>
2016-03-27 19:09:02 +0430

    Updates
    Signed-off-by: ePay.info <info@epay.info>

--- maincore.php
+++ maincore.php
@@ -8,12 +8,12 @@ require_once "configs/dbinfo.php";
 if( !class_exists('SystemComponent') )
 	header('Location: install.php');
 
-
-
 require_once "includes/dbconnector.class.php";
 require_once "includes/smarty/Smarty.class.php";
 require_once "includes/function.php";
 require_once "includes/solvemedialib.php";
+require_once "includes/captcha.ws.php";
+require_once "includes/recaptchalib.php";
 require_once "configs/configs.php";
 require_once "configs/prizes.php";
 /*** Includes ***/
@@ -33,6 +33,7 @@ $ads_main_top=file_get_contents('ads/top.php');
 $ads_right=file_get_contents('ads/right.php');
 $ads_1=file_get_contents('ads/ad1.php');
 $ads_2=file_get_contents('ads/ad2.php');
+$ads_3=file_get_contents('ads/ad3.php');
 $ads_main_bottom=file_get_contents('ads/bottom.php');
 /***** ADS ****/
 $smarty->assign('footer',$footer);
