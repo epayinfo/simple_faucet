0ad36fc7222f5efc774aef05c4c8cc801ca78001
ePay.info <info@epay.info>
2016-03-27 19:12:30 +0430

    New features
    Signed-off-by: ePay.info <info@epay.info>

--- configs/configs.php
+++ configs/configs.php
@@ -1,13 +1,20 @@
 <?php
+$adb='1';
+$bwait='10';
+$currency='1';
 $apicode='your api code';
-$privkey='private key';
-$verkey='verification key';
-$hashkey='hash key';
-$reflimit='1000';
+$privkey='';
+$verkey='';
+$hashkey='';
 $ref_percent='50';
 $sitetitle='Your faucet title here';
 $domainname='youdomain.com';
 $setinterval='10';
 $keywords='Sitekeywords';
 $desc='Your faucet description here';
-?>
+$captcha_ws_active='0';
+$captcha_ws_key='';
+$solvemedia_active='0';
+$recap_active='0';
+$recap_site='';
+$recap_secret='';
